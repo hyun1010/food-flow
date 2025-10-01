98fe407df326da906f3aba86886c21b8f334c6ec
ChyunKim <chkim@realsn.com>
2025-10-02 08:54:17 +0900

    feat: 컴포넌트 기본 생성

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,11 @@
+import { Button } from "@repo/ui/button";
+import { Card } from "@repo/ui/card";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Button />
+      <Card />
+    </div>
+  );
 }
---
+++ packages/ui/src/card/card.tsx
@@ -0,0 +1,5 @@
+export default function Card() {
+  return <div>Card</div>;
+}
+
+Card.displayName = "Card";
